04b147f9b5d7c98d948d4ad9550612201097b9c8
Laura DeCicco <ldecicco@usgs.gov>
2018-05-29 13:56:04 -0500

    classes and exclusions

--- get_data_ready.R
+++ get_data_ready.R
@@ -22,6 +22,7 @@
 library(readxl)
 library(tidyr)
 library(dplyr)
+library(openxlsx)
 
 file_2014 <- "rawData/GLRI passive sampler data update 3-17-16.xlsx"
 pharm_file <- "rawData/GLRI passive sampler pharmaceutical data 8-23-17.xlsx"
@@ -81,7 +82,7 @@ generic_file_opener <- function(file_name, n_max, sheet, year, file_cas=file_cas
   data_long$Value <- gsub(" ","",data_long$Value)
   data_long$Value <- as.numeric(data_long$Value) 
   data_long$Value <- data_long$Value/convert
-  
+  data_long$generic_class <- sheet
   data_long$`Sample Date` <- year
   
   data_long <- filter(data_long, 
@@ -122,6 +123,8 @@ data_2010_PAHs <- generic_file_opener(file_2010,
                                       year = 2010,
                                       file_cas = file_cas)
 # "Benzo[g,h,i]perylene" didn't get a CAS
+data_2010_PAHs$chnm[data_2010_PAHs$chnm == "Benzo[g,h,i]perylene"] <- "Benzo[g,h,I]perylene"
+data_2010_PAHs$CAS[data_2010_PAHs$chnm == "Benzo[g,h,I]perylene"] <- "191-24-2"
 
 #####################################################
 # OC-PCB-PBDE 2010
@@ -155,26 +158,70 @@ data_2010_Pharm <- generic_file_opener(file_2010,
                                     year = 2010,
                                     file_cas = file_cas)
 
-
 #####################################################
 # Sites:
-sites <- read_excel(file_2014,
+sites_orig <- read_excel(file_2014,
                      sheet = "site info",
                      skip = 3) 
 
-sites <- sites %>%
-  rename(site = `CERC Site #`,
+sites_orig <- sites_orig %>%
+  rename(SiteID = `CERC Site #`,
          site_grouping = Lake,
          `Short Name` = `Station shortname`) %>%
-  mutate(site = paste("Site",site),
+  mutate(SiteID = paste("Site",SiteID),
          STAID = dataRetrieval::zeroPad(STAID, 8)) %>%
-  select(site, site_grouping, `Short Name`, STAID)
+  select(SiteID, site_grouping, `Short Name`, STAID)
 
-full_sites <- dataRetrieval::readNWISsite(sites$STAID[1:49])
+full_sites <- dataRetrieval::readNWISsite(sites_orig$STAID[1:49])
 
-sites <- sites %>%
-  left_join(select(full_sites, STAID=site_no, station_nm, 
+sites <- sites_orig %>%
+  left_join(select(full_sites, STAID=site_no, Fullname=station_nm, 
                    dec_lat=dec_lat_va, dec_lon = dec_long_va), by="STAID")
 
+site_na <- dataRetrieval::zeroPad(sites$STAID[is.na(sites$dec_lon)],9)
+site_na <- site_na[substr(site_na, start = 1, stop = 2) != "00"]
+names(site_na) <- substr(site_na, start=2, stop = nchar(site_na))
+more_sites <- dataRetrieval::readNWISsite(site_na)
+
+sites_orig$STAID[sites_orig$STAID %in% names(site_na)] <- site_na
+
+sites <- sites_orig %>%
+  left_join(select(bind_rows(full_sites, more_sites), 
+                   STAID=site_no, Fullname=station_nm, 
+                   dec_lat=dec_lat_va, dec_lon = dec_long_va), by="STAID")
+
+sites <- filter(sites, STAID != "000-----")
+
+
+##############################################
+# Bring it together:
+all_data <- bind_rows(data_2010_Pharm,
+                      data_2010_WW,
+                      data_2010_OC,
+                      data_2010_PAHs,
+                      data_2014_OC,
+                      data_2014_PAHs)
+
+chem_data <- all_data %>%
+  select(SiteID, `Sample Date`, CAS, Value)
+
+chem_info_old <- read.csv("data/chem_classes.csv", stringsAsFactors = FALSE)
+
+chem_info <- select(all_data, CAS, generic_class) %>%
+  distinct() %>%
+  left_join(chem_info_old, by="CAS")
+
+chem_info$Class[is.na(chem_info$Class)] <- chem_info$generic_class[is.na(chem_info$Class)]
+
+exclude <- read.csv("data/exclude.csv", stringsAsFactors = FALSE)
+
+dir.create("cleanedData",showWarnings = FALSE)
+
+list_of_datasets <- list("Data" = chem_data, 
+                         "Chemicals" = chem_info,
+                         "Sites" = sites,
+                         "Exclude" = exclude)
+
+write.xlsx(list_of_datasets, file = "cleanedData/passive.xlsx", append=TRUE)
 
 
---
+++ toxEval_workflow.R
@@ -0,0 +1,20 @@
+library(toxEval)
+tox_list <- create_toxEval("cleanedData/passive.xlsx")
+
+ACClong <- get_ACC(tox_list$chem_info$CAS)
+ACClong <- remove_flags(ACClong)
+
+cleaned_ep <- clean_endPoint_info(endPointInfo)
+filtered_ep <- filter_groups(cleaned_ep)
+
+lakes_ordered <- c("Lake Superior",
+                   "Lake Michigan",
+                   "Lake Huron",
+                   "Lake Erie",
+                   "Lake Ontario")
+
+tox_list$chem_site$site_grouping <- factor(tox_list$chem_site$site_grouping,
+                                           levels=lakes_ordered)
+chemicalSummary <- get_chemical_summary(tox_list, ACClong, filtered_ep)
+
+plot_tox_boxplots(chemicalSummary, category = "Chemical")

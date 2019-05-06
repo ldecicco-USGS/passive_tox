5c85152c1687c1bb06bbbd77de62276cbc7e3cf0
DeCicco <ldecicco@usgs.gov>
2019-05-06 10:50:29 -0500

    Put back the astricks!

--- R/report/combo_plot2.R
+++ R/report/combo_plot2.R
@@ -1,107 +1,44 @@
-combo_plot_matches_2 <- function(gd_1, gd_2,
-                               thres_1, thres_2,
-                               drop = TRUE,
-                               gd_3=NULL,thres_3=NA,
-                               grid = FALSE,
-                               axis_size = 6){
-  
+combine_gd <- function(gd_1, gd_2){
   
   guide_side_1 <- gd_1$guide_side[1]
   guide_side_2 <- gd_2$guide_side[1]
   
-  if(all(is.null(gd_3))){
-    if(length(levels(gd_1$chnm)) > 0){
-        orderChem_1_2 <- bind_rows(gd_1,
+  if(length(levels(gd_1$chnm)) > 0){
+    orderChem_1_2 <- bind_rows(gd_1,
                                filter(gd_2, !(chnm %in% levels(gd_1$chnm)))) %>%
-          group_by(chnm,Class) %>%
-          summarise(median = quantile(meanEAR[meanEAR != 0],0.5)) %>%
-          ungroup()
-    } else {
-      combine_data <- gd_2 %>%
-        filter(!(CAS %in% unique(gd_1$CAS))) %>%
-        mutate(Class = as.character(Class),
-               chnm = as.character(chnm)) %>%
-        bind_rows(mutate(gd_1, 
-                         Class = as.character(Class),
-                         chnm = as.character(chnm)))
-      
-      orderChem_1_2 <- combine_data %>%
-        group_by(chnm,Class) %>%
-        summarise(median = quantile(meanEAR[meanEAR != 0],0.5)) %>%
-        ungroup()
-    }
-    
-    if(all(levels(gd_2$Class) %in% levels(gd_1$Class)) &
-       all(levels(gd_1$Class) %in% levels(gd_2$Class))){
-      class_order <- levels(gd_1$Class)
-    } else {
-      class_order <- combine_data %>%
-        toxEval:::orderClass() %>%
-        pull(Class)
-    }
-    
-    graphData_1_2 <- bind_rows(mutate(gd_1,
-                                      Class = as.character(Class),
-                                      chnm = as.character(chnm)),
-                               mutate(gd_2,
-                                      Class = as.character(Class),
-                                      chnm = as.character(chnm)))
-    
-    countNonZero_1_2 <- graphData_1_2 %>%
-      group_by(site, chnm, Class, guide_side) %>%
-      summarise(meanEAR = mean(meanEAR, na.rm=TRUE)) %>%
-      group_by(chnm, Class, guide_side) %>%
-      summarise(nonZero = as.character(sum(meanEAR>0)),
-                hits = as.character(sum(meanEAR > ifelse(guide_side == guide_side_1, thres_1, thres_2)))) %>%
-      ungroup() 
-
-  } else {
-    graphData_1_2 <- bind_rows(mutate(gd_1,
-                                      Class = as.character(Class),
-                                      chnm = as.character(chnm)),
-                               mutate(gd_2,
-                                      Class = as.character(Class),
-                                      chnm = as.character(chnm)))
-    
-    orderChem_1_2 <- bind_rows(mutate(gd_1,
-                                      Class = as.character(Class),
-                                      chnm = as.character(chnm)),
-                               filter(mutate(gd_2,
-                                             Class = as.character(Class),
-                                             chnm = as.character(chnm)),
-                                      !(chnm %in% levels(gd_1$chnm)))) %>%
-      group_by(CAS,Class) %>%
+      group_by(chnm,Class) %>%
       summarise(median = quantile(meanEAR[meanEAR != 0],0.5)) %>%
-      ungroup() 
+      ungroup()
+  } else {
+    combine_data <- gd_2 %>%
+      filter(!(CAS %in% unique(gd_1$CAS))) %>%
+      mutate(Class = as.character(Class),
+             chnm = as.character(chnm)) %>%
+      bind_rows(mutate(gd_1, 
+                       Class = as.character(Class),
+                       chnm = as.character(chnm)))
     
-    class_order <- toxEval:::orderClass(bind_rows(gd_1, 
-                                                  filter(gd_2, !(chnm %in% levels(gd_1$chnm))),
-                                                  filter(gd_3, !(chnm %in% c(levels(gd_1$chnm),levels(gd_2$chnm)))))) %>%
-      pull(Class)
-    
-    graphData_1_2 <- bind_rows(mutate(gd_1,
-                                      Class = as.character(Class),
-                                      chnm = as.character(chnm)),
-                               mutate(gd_2,
-                                      Class = as.character(Class),
-                                      chnm = as.character(chnm)), 
-                               mutate(gd_3,
-                                      Class = as.character(Class),
-                                      chnm = as.character(chnm)))
-    guide_side_3 <- gd_3$guide_side[1]
-    
-    countNonZero_1_2 <- graphData_1_2 %>%
-      group_by(site, chnm, Class, guide_side, guide_up) %>%
-      summarise(meanEAR = mean(meanEAR, na.rm=TRUE)) %>%
-      group_by(chnm, Class, guide_side, guide_up) %>%
-      summarise(nonZero = as.character(sum(meanEAR>0)),
-                hits = as.character(sum(meanEAR > ifelse(guide_side == guide_side_1, thres_1, thres_2)))) %>%
-      ungroup() 
-    
-    class_order <- toxEval:::orderClass(bind_rows(gd_1, 
-                                                  filter(gd_2, !(chnm %in% unique(gd_1$chnm))),
-                                                  filter(gd_3, !(chnm %in% c(unique(gd_1$chnm),unique(gd_2$chnm))))))
+    orderChem_1_2 <- combine_data %>%
+      group_by(chnm,Class) %>%
+      summarise(median = quantile(meanEAR[meanEAR != 0],0.5)) %>%
+      ungroup()
   }
+  
+  if(all(levels(gd_2$Class) %in% levels(gd_1$Class)) &
+     all(levels(gd_1$Class) %in% levels(gd_2$Class))){
+    class_order <- levels(gd_1$Class)
+  } else {
+    class_order <- combine_data %>%
+      toxEval:::orderClass() %>%
+      pull(Class)
+  }
+  
+  graphData_1_2 <- bind_rows(mutate(gd_1,
+                                    Class = as.character(Class),
+                                    chnm = as.character(chnm)),
+                             mutate(gd_2,
+                                    Class = as.character(Class),
+                                    chnm = as.character(chnm)))
   
   orderChem_1_2 <- orderChem_1_2 %>%
     mutate(Class = factor(Class, levels=class_order)) %>%
@@ -111,55 +48,13 @@ combo_plot_matches_2 <- function(gd_1, gd_2,
   graphData_1_2$chnm <- factor(graphData_1_2$chnm, 
                                levels = unique(orderChem_1_2$chnm))
   
-  if(drop){
-    if(grid){
-      chems_in_A <- filter(graphData_1_2, guide_up == gd_2$guide_up[1]) %>%
-        select(guide_side, chnm) %>%
-        distinct() 
-      
-      chems_in_A <- chems_in_A$chnm[duplicated(chems_in_A$chnm)]
-      if(length(chems_in_B) > 0){
-        chems_in_A <- data.frame(chnm = chems_in_A, guide_up = gd_2$guide_up[1], stringsAsFactors = FALSE)
-      } else {
-        chems_in_A <- data.frame(chnm = NA, guide_up = gd_2$guide_up[1], stringsAsFactors = FALSE)
-      }
-      
-      chems_in_B <- filter(graphData_1_2, guide_up == gd_3$guide_up[1]) %>%
-        select(guide_side, chnm) %>%
-        distinct()
-      
-      chems_in_B <- chems_in_B$chnm[duplicated(chems_in_B$chnm)]
-      if(length(chems_in_B) > 0){
-        chems_in_B <- data.frame(chnm=chems_in_B, guide_up = gd_3$guide_up[1], stringsAsFactors = FALSE)
-      } else {
-        chems_in_B <- data.frame(chnm = NA, guide_up = gd_2$guide_up[1], stringsAsFactors = FALSE)
-      }
-      
-      chems_in <- bind_rows(chems_in_A, chems_in_B)
-      
-      graphData_1_2 <- graphData_1_2 %>%
-        right_join(chems_in, by=c("chnm","guide_up"))
-    } else {
-      chems_in_1_2 <- graphData_1_2 %>%
-        select(guide_side, chnm) %>%
-        distinct() 
-      
-      chems_in_1_2 <- chems_in_1_2$chnm[duplicated(chems_in_1_2$chnm)]
-      
-      graphData_1_2 <- filter(graphData_1_2, chnm %in% chems_in_1_2$chnm)
-    }
-    
-  }
-
-  thresh_df <- data.frame(guide_side = c(guide_side_1,guide_side_2),
-                          thres = c(thres_1, thres_2),
-                          stringsAsFactors = FALSE)
-  
-  thresh_df <- thresh_df[!is.na(thresh_df$thres),]
-  
   graphData_1_2$guide_side <- factor(graphData_1_2$guide_side, levels = c(guide_side_1,guide_side_2))
-  thresh_df$guide_side <- factor(thresh_df$guide_side, levels = c(guide_side_1,guide_side_2))
-  countNonZero_1_2$guide_side <- factor(countNonZero_1_2$guide_side, levels = c(guide_side_1,guide_side_2))
+  
+  return(graphData_1_2)
+}
+
+combo_plot_matches_2 <- function(graphData_1_2,
+                               axis_size = 6){
   
   cbValues <- c("#E41A1C","#377EB8","#4DAF4A","#984EA3","#FF7F00","#FFFF33","#A65628",
                 "#DCDA4B","#999999","#00FFFF","#CEA226","#CC79A7","#4E26CE",
@@ -193,25 +88,9 @@ combo_plot_matches_2 <- function(gd_1, gd_2,
           panel.grid.minor = element_blank()) +
     scale_fill_manual(values = cbValues, drop=FALSE) +
     scale_color_manual(values = c("black","red"), 
-                       na.value = "black", guide = "none")
-  if(!drop){
-    toxPlot_1_2 <- toxPlot_1_2 +
-      scale_x_discrete(drop=TRUE) 
-  }
-  
-  if(grid){
-    toxPlot_1_2 <- toxPlot_1_2 +
-      facet_grid(guide_up ~ guide_side, 
-                 scales = "free",
-                 space = "free_y", 
-                 labeller = label_parsed)+
-      theme(strip.text.y = element_blank())
-    
-  } else {
-    toxPlot_1_2 <- toxPlot_1_2 +
-      facet_grid(. ~ guide_side, scales = "free")
-  }
-  
+                       na.value = "black", guide = "none") +
+    facet_grid(. ~ guide_side, scales = "free") 
+
   return(toxPlot_1_2)
   
 }
@@ -250,18 +129,67 @@ graph_chem_data_CAS <- function(chemical_summary,
   return(graphData)
 }
 
-add_label <- function(plot_full, labels_to_use = c("A","B")){
+add_astrict <- function(plot_full, graphData_1_2){
   
-  plot_data <- plot_full$data
+  countNonZero_1_2 <- graphData_1_2 %>%
+    group_by(site, chnm, Class, guide_side) %>%
+    summarise(meanEAR = mean(meanEAR, na.rm=TRUE)) %>%
+    group_by(chnm, Class, guide_side) %>%
+    summarise(nonZero = as.character(sum(meanEAR>0))) %>%
+    ungroup() 
 
-  textData <- plot_data %>%
-    group_by(guide_up, guide_side) %>%
-    summarize(chnm = tail(levels(chnm),1),
+  min_ys <- graphData_1_2 %>%
+    filter(meanEAR > 0) %>%
+    group_by(guide_side) %>%
+    summarize(y = min(meanEAR, na.rm = TRUE))
+  
+  chm_side_A <- graphData_1_2 %>%
+    select(chnm, Class) %>%
+    distinct() %>%
+    mutate(guide_side = levels(graphData_1_2$guide_side)[1],
+           guide_side = factor(guide_side, levels = levels(graphData_1_2$guide_side)))
+
+  chm_side_B <- graphData_1_2 %>%
+    select(chnm, Class) %>%
+    distinct() %>%
+    mutate(guide_side = levels(graphData_1_2$guide_side)[2],
+           guide_side = factor(guide_side, levels = levels(graphData_1_2$guide_side)))
+  
+  chm_side <- bind_rows(chm_side_A, chm_side_B) %>%
+    left_join(min_ys, by="guide_side")
+  
+  countNonZero_1_2 <- right_join(countNonZero_1_2, 
+                                 chm_side, 
+                                 by=c("chnm","Class","guide_side"))
+  
+  countNonZero_1_2$nonZero[is.na(countNonZero_1_2$nonZero)] <- "*"
+  
+  countNonZero_1_2 <- countNonZero_1_2 %>%
+    filter(nonZero == "*")
+  
+  plot_full_w_ast <- plot_full +
+    geom_text(data=countNonZero_1_2, size=2.5, color="blue",
+              aes(x= chnm, label = nonZero, y = y)) 
+  
+  return(plot_full_w_ast)
+  
+}
+
+label_info <- function(gd1_2, labels_to_use = c("A","B")){
+  
+  textData <- gd1_2 %>%
+    group_by(guide_side) %>%
+    summarize(chnm = factor(tail(levels(chnm),1), levels = levels(chnm)),
               y = max(meanEAR, na.rm = TRUE)) %>%
     mutate(textExplain = labels_to_use)
   
+  return(textData)
+}
+
+add_label <- function(plot_full, label_info){
+  
   plot_full_w_label <- plot_full +
-    geom_text(data = textData, 
+    geom_text(data = label_info, 
               aes(label = textExplain, x = chnm, y=y),size = 2)
   
   return(plot_full_w_label)
@@ -287,7 +215,7 @@ site_counts <- function(chem_data, plot_data){
     group_by(CAS) %>%
     summarise(nSites = length(unique(SiteID))) %>%
     ungroup() %>%
-    right_join(distinct(select(plot_data, CAS, Class, chnm, guide_up)), by="CAS") %>%
+    right_join(distinct(select(plot_data, CAS, Class, chnm)), by="CAS") %>%
     mutate(guide_side = "Sites")
   
   site_counts$nSites[is.na(site_counts$nSites)] <- 0
@@ -301,7 +229,7 @@ site_count_plot <- function(site_counts, axis_size = 6){
   
   site_graph <- ggplot(data = site_counts) +
     geom_text(aes(x=chnm, label = nSites, y=100), size = 2) +
-    facet_grid(guide_up ~ guide_side, scales = "free", space = "free_y")+
+    facet_grid(. ~ guide_side, scales = "free", space = "free_y")+
     theme_bw() +
     coord_flip() +
     scale_y_log10(labels=toxEval:::fancyNumbers,breaks=pretty_logs_new)  +
@@ -323,20 +251,21 @@ site_count_plot <- function(site_counts, axis_size = 6){
   return(site_graph)
 }
 
-fancy_combo <- function(graphData_tox, graphData_conc, tox_list, axis_size = 6){
+fancy_combo <- function(gd1, gd2, tox_list, axis_size = 6){
   
-  toxPlot_wq <- combo_plot_matches_2(graphData_tox, 
-                                     graphData_conc, 
-                                     thres_1 = NA, 
-                                     thres_2 = NA, 
-                                     drop = FALSE,
-                                     axis_size = axis_size)
+  gd1_2 <- combine_gd(gd_1 = gd1, 
+                      gd_2 = gd2)
   
-  toxPlot_wq_w_lab <- add_label(toxPlot_wq)
+  toxPlot_wq <- combo_plot_matches_2(gd1_2,
+                                     axis_size = 6)
+  ast_plot <- add_astrict(toxPlot_wq, gd1_2)
+  text_df <- label_info(gd1_2)
+  toxPlot_wq_w_lab <- add_label(ast_plot, text_df)
   no_axis <- strip_graph(toxPlot_wq_w_lab)
   site_counts_df <- site_counts(tox_list$chem_data, no_axis$data)
   site_graph <- site_count_plot(site_counts_df)
     
-  return(list(no_axis=no_axis, site_graph=site_graph))
+  return(list(no_axis=no_axis, 
+              site_graph=site_graph))
 }
 

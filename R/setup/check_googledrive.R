6d9d641811d7493b5dff460a8ba9c5986dfd719a
DeCicco <ldecicco@usgs.gov>
2019-06-14 14:01:49 -0500

    Better drake workflow

--- R/setup/check_googledrive.R
+++ R/setup/check_googledrive.R
@@ -1,6 +1,6 @@
 # Modified from:
 # https://github.com/ropensci/drake/issues/252
-
+# https://drive.google.com/drive/folders/1jLF5L4WzyON7JLhf0BRLj2a_wU3NK5Vm
 drive_get_datetime_modified <- function(dr_id){
 
   datetime_modified <- dr_id %>% 

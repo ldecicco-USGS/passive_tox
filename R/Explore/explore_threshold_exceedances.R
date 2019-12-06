243fa028f76cfa368b81218c108beff4810f298b
srcorsi-USGS <srcorsi@usgs.gov>
2019-12-06 16:03:25 -0600

    count chemical presence and exceedances for text

--- R/Explore/explore_threshold_exceedances.R
+++ R/Explore/explore_threshold_exceedances.R
@@ -9,6 +9,7 @@ library(tidyverse)
 #NOTE: Add path to path_to_file!!!
 path_to_file <- 'data/clean/passive.xlsx' 
 tox_list <- create_toxEval(path_to_file)
+chems <- tox_list$chem_info
 ACC <- get_ACC(tox_list$chem_info$CAS)
 ACC <- remove_flags(ACC = ACC,
                     flagsShort = c('Borderline','OnlyHighest','GainAC50','Biochemical'))
@@ -21,6 +22,7 @@ filtered_ep <- filter_groups(cleaned_ep,
 
 chemical_summary <- get_chemical_summary(tox_list, ACC, filtered_ep)
 
+#How many endpoints per chemical?
 how_many_eps_per_chem <-group_by(chemical_summary,site,chnm,) %>%
   summarize(num_eps = length(unique(EAR))) %>%
   group_by(chnm) %>%
@@ -28,7 +30,8 @@ how_many_eps_per_chem <-group_by(chemical_summary,site,chnm,) %>%
   arrange(as.character(chnm))
 
 
-##
+## How many sites were chems present and how many of those had EAR > 0.001?
+
 max_by_EP <-group_by(chemical_summary,site,chnm,endPoint) %>%
   summarize(max_EAR_EP = max(EAR))
 
@@ -41,6 +44,9 @@ site_exceed_by_EP <- group_by(max_by_EP,chnm,endPoint) %>%
   summarize(num_sites_exceeded = sum(max_EAR_EP > thresh),
             num_sites_present = sum(max_EAR_EP > 0)) %>%
   filter(num_sites_exceeded >= site_thresh & num_sites_present >= site_occur_thresh)
+
+##!!!! work out chnm name matching. Might need to use CAS !!!###
+sum(chems$chnm %in% unique(site_exceed_by_EP$chnm))
 
 ggplot(data = site_exceed_by_EP,aes(x=chnm,y=num_sites_exceeded)) +
   geom_point() +
@@ -55,6 +61,8 @@ ggplot(data = site_exceed_by_EP,aes(x=endPoint,y=num_sites_exceeded)) +
   coord_flip() +
   ggtitle(label = plot_title)
 
+
+#Take a look at endopints and what intended target families they represent. 
 site_exceed_by_EP$endPoint %in% x$assay_component_name
 
 x_CellCycle <- toxEval::end_point_info %>% filter(intended_target_family == "cell cycle")
@@ -63,6 +71,7 @@ site_exceed_by_EP$endPoint %in% x_CellCycle$assay_component_endpoint_name
 x_all <- toxEval::end_point_info 
 y <- left_join(site_exceed_by_EP,x_all,by=c("endPoint" = "assay_component_endpoint_name"))
 y$intended_target_family
+unique(y$intended_target_family)
 
 site_exceed_by_EP$endPoint %in% x_all$assay_component_endpoint_name
 
@@ -82,12 +91,7 @@ exceed_by_EP <- group_by(max_by_EP,site,chnm) %>%
 
 
 
-
-
-DL <- filter(chemical_summary,chnm == "D-Limonene") %>%
-  filter(EAR >= thresh)
-
-
-plot(as.factor(as.character(site_exceed_by_EP$chnm)),site_exceed_by_EP$num_sites_exceeded,las=2)
-
-plot(exceed_by_EP$
+# How many chemicals with EAR > 0.001 for manuscript text
+chems_with_exceedances <- filter(chemical_summary,EAR >= 0.001)
+unique(chems_with_exceedances$chnm)  
+length(unique(chems_with_exceedances$chnm)  )

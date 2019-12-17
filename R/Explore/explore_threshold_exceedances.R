41ee4ddccb5e0ef1b470a4a853d84d32e1df8944
srcorsi-USGS <srcorsi@usgs.gov>
2019-12-17 16:25:25 -0600

    exploring for manuscript text

--- R/Explore/explore_threshold_exceedances.R
+++ R/Explore/explore_threshold_exceedances.R
@@ -1,7 +1,3 @@
-library(toxEval)
-
-explore_endpoints()
-
 
 library(toxEval)
 library(tidyverse)
@@ -10,6 +6,7 @@ library(tidyverse)
 path_to_file <- 'data/clean/passive.xlsx' 
 tox_list <- create_toxEval(path_to_file)
 chems <- tox_list$chem_info
+conc <- tox_list$chem_data
 ACC <- get_ACC(tox_list$chem_info$CAS)
 ACC <- remove_flags(ACC = ACC,
                     flagsShort = c('Borderline','OnlyHighest','GainAC50','Biochemical'))
@@ -22,6 +19,46 @@ filtered_ep <- filter_groups(cleaned_ep,
 
 chemical_summary <- get_chemical_summary(tox_list, ACC, filtered_ep)
 
+
+## EARchem summations ##
+
+EAR_sums <- chemical_summary %>% 
+  group_by(site, date,chnm,CAS) %>%
+  summarise(sumEAR = sum(EAR)) %>%
+  group_by(site,chnm,CAS) %>% 
+  summarize(maxEAR = max(sumEAR)) %>% 
+  filter(maxEAR > 0)
+
+EAR_exceedances <- filter(EAR_sums, maxEAR > 0.001)
+length(unique(EAR_exceedances$site))
+length(unique(EAR_exceedances$chnm)) #32 chemicals have EARchem > 0.001 for at least one sample
+
+## Determine how many chems have EARchem > 0.001 for at least XX sites
+thresh <- 0.001
+site_thresh <- 10
+
+
+#Number of sites per endpoint per chemical that exceed thresh
+site_exceed <- EAR_sums %>% group_by(chnm, CAS,) %>%
+  summarize(num_sites_exceeded = sum(maxEAR > thresh)) %>%
+  filter(num_sites_exceeded >= site_thresh)
+  # 11 sites have EARchem > 0.001 for 10 or more sites
+site_exceed$chnm
+
+
+unique(chemical_summary$site)
+
+# Why menthol??
+menthol <- chemical_summary %>% 
+  filter(CAS == "89-78-1")
+unique(menthol$endPoint)
+
+## End of EARchem summations ##
+#############################################
+
+
+## Individual endpoint exploration ##
+
 #How many endpoints per chemical?
 how_many_eps_per_chem <-group_by(chemical_summary,site,chnm,) %>%
   summarize(num_eps = length(unique(EAR))) %>%
@@ -32,21 +69,24 @@ how_many_eps_per_chem <-group_by(chemical_summary,site,chnm,) %>%
 
 ## How many sites were chems present and how many of those had EAR > 0.001?
 
-max_by_EP <-group_by(chemical_summary,site,chnm,endPoint) %>%
+max_by_EP <-group_by(chemical_summary,site,chnm,CAS,endPoint) %>%
   summarize(max_EAR_EP = max(EAR))
 
 thresh <- 0.001
-site_thresh <- 5
-site_occur_thresh <- 10
+site_thresh <- 2
+site_occur_thresh <- 2
 
 #Number of sites per endpoint per chemical that exceed thresh
-site_exceed_by_EP <- group_by(max_by_EP,chnm,endPoint) %>%
+site_exceed_by_EP <- group_by(max_by_EP,chnm,CAS,endPoint) %>%
   summarize(num_sites_exceeded = sum(max_EAR_EP > thresh),
             num_sites_present = sum(max_EAR_EP > 0)) %>%
   filter(num_sites_exceeded >= site_thresh & num_sites_present >= site_occur_thresh)
 
+unique(site_exceed_by_EP$chnm)
+
 ##!!!! work out chnm name matching. Might need to use CAS !!!###
-sum(chems$chnm %in% unique(site_exceed_by_EP$chnm))
+sum(unique(as.character(site_exceed_by_EP$CAS)) %in% chems$CAS)
+priority_individual_chems <- filter(chems,CAS %in% site_exceed_by_EP$CAS)
 
 ggplot(data = site_exceed_by_EP,aes(x=chnm,y=num_sites_exceeded)) +
   geom_point() +
